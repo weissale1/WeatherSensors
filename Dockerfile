4fd01d552c4d3a4f9b8be3c4df5f3e0ceddf5193
weissale1 <alexander.weiss95@outlook.com>
2024-06-17 14:39:58 +0200

    move to app.py

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,5 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 EXPOSE 5000
 
-ENV FLASK_APP=WeatherStation2
-
-# Run app.py when the container launches
-CMD ["flask", "run", "--host=0.0.0.0"]
+# Run flask when the container launches
+CMD ["python3", "-m", "flask", "run", "--host=0.0.0.0"]
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+from .api import current_weather
+from .services import db
+
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.register_blueprint(current_weather.bp)
+    db.init_app(app)
+    
+    return app
